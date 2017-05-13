7ff938195950fd595b16b89621bced0cfe58460e
KhaledGamal <khaledgamal@KhaledGamal.localdomain>
2017-05-13 10:46:49 +0200

    adding the relations models

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,7 +23,6 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Nelmio\CorsBundle\NelmioCorsBundle(),
             new Endroid\QrCode\Bundle\EndroidQrCodeBundle(),
-            new Limenius\ReactBundle\LimeniusReactBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
