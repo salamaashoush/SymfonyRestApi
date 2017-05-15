717aedb7882e36c2ca1c651614d887542e0d84f0
abdelrahman <abdelrehman.salah@gmail.com>
2017-05-16 01:38:14 +0200

    student ask for permission

--- src/AppBundle/Controller/Api/AttendanceController.php
+++ src/AppBundle/Controller/Api/AttendanceController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller\Api;
 
 use AppBundle\Entity\Students_Attendance;
+use AppBundle\Entity\Students_Abscence;
 use AppBundle\Entity\Rule;
 use AppBundle\Entity\User;
 use FOS\RestBundle\Controller\FOSRestController;
@@ -11,11 +12,10 @@ use Symfony\Component\HttpFoundation\Request;
 class AttendanceController extends FOSRestController
 {
   /**
-   * @param User $student
    * @param Request $request
    * @return array
    */
-  public function postStudentAttendanceAction(User $student,Request $request)
+  public function postStudentAttendanceAction(Request $request)
   {
       $user = $this->getUser();
       $user_id = $user->getId();
@@ -46,9 +46,45 @@ class AttendanceController extends FOSRestController
         return $this->view(['Message' => 'you have signed your Attendance Successfully', 'Success' => true], Response::HTTP_CREATED);
       }
       else {
-        return $this->view(['Message' => 'you have already signed your Attendance before', 'Success' => false], Response::HTTP_CREATED);
+        return $this->view(['Message' => 'you have already signed your Attendance before', 'Success' => false], Response::HTTP_NOT_ACCEPTABLE);
       }
   }
+
+  public function postStudentPermission(User $student,$permissonStatus,$permissiondate,Request $request){
+    $user_id = $student->getId();
+    $trackId = $student->getTrackId();
+    $studentAbscence = new Students_Abscence();
+    $studentAbscence->setUser($user_id);
+    $studentAbscence->setTrack($trackId);
+    $ruleid = 0;
+    $repository = $this->getDoctrine()->getRepository('AppBundle:Students_Abscence');
+    if ($permissonStatus == "Absent")
+    {
+      $rule = $repository->findOneBy('Abscence_Status' =>'Abscence With Permission');
+      $ruleid = $rule->getId();
+    }elseif ($permissonStatus == "Late"){
+      $rule = $repository->findOneBy('Abscence_Status' =>'Late With Permission');
+      $ruleid = $rule->getId();
+    }else {
+      $repository = $this->getDoctrine()->getRepository('AppBundle:Students_Attendance');
+      $student = $repository->findOneBy(array('user_id' =>$user_id, 'track_id' => $trackId, 'status' => 1,'arrival_time'=>$permissiondate));
+      if($student)
+      {
+        $rule = $repository->findOneBy('Abscence_Status' =>'Leave With Permission');
+        $ruleid = $rule->getId();
+      }else {
+          return $this->view(['Message' => 'you can not have a leave permission','Success' => false], Response::HTTP_NOT_ACCEPTABLE);
+      }
+    }
+    $studentAbscence->setRule($ruleid);
+    $studentAbscence->setAbscenceDate($permissiondate);
+    $em = $this->getDoctrine()->getManager();
+    $em->persist($studentAbscence);
+    $em->flush();
+    return $this->view(['Message' => 'your permission Submitted Successfully', 'Success' => true], Response::HTTP_CREATED);
+  }
+
+  
 
   public function getStudentAttendanceAction(Request $request,$trackID)
   {
