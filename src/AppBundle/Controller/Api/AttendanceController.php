d7785a3b08d84d34ea9e595891901093a15c1dda
abdelrahman <abdelrehman.salah@gmail.com>
2017-05-15 22:38:36 +0200

    student  attendance sigin

--- src/AppBundle/Controller/Api/AttendanceController.php
+++ src/AppBundle/Controller/Api/AttendanceController.php
@@ -3,8 +3,56 @@
 namespace AppBundle\Controller\Api;
 
 use AppBundle\Entity\Rules;
+use AppBundle\Entity\Students_Attendance;
 use FOS\RestBundle\Controller\FOSRestController;
 
 class AttendanceController extends FOSRestController
 {
+  public function postStudentAttendanceAction(Request $request)
+  {
+      $user = $this->getUser();
+      $user_id = $user->getId();
+      $trackId = $user->getTrackId()
+      $repository = $this->getDoctrine()->getRepository('AppBundle:Students_Attendance');
+      $student = $repository->findOneBy(array('user_id' =>$user_id, 'track_id' => $trackId));
+      if($student)
+      {
+        $studentAttendance = new Students_Attendance();
+        $Latedate = new DateTime();
+        $Latedate->setTime(9,15);
+        $Abscencedate = new DateTime();
+        $Abscencedate->setTime(10,30);
+        $currentDate = new DateTime();
+        if ($Latedate <= $currentDate)
+        {
+           $studentAttendance->setStatus(1);
+        }elseif ($Latedate >= $currentDate && $Abscencedate <= $currentDate)
+        {
+          $studentAttendance->setStatus(0);
+        }
+        $studentAttendance->setArrivalTime(new \DateTime());
+        $studentAttendance->setTrack($trackId);
+        $studentAttendance->setUser($user_id);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($studentAttendance);
+        $em->flush();
+        return $this->view(['Message' => 'you have signed your Attendance Successfully', 'Success' => true], Response::HTTP_CREATED);
+      }
+      else {
+        return $this->view(['Message' => 'you have already signed your Attendance before', 'Success' => false], Response::HTTP_CREATED);
+      }
+  }
+
+  public function getStudentAttendanceAction(Request $request,$trackID)
+  {
+    // return only late or absent students
+    $repository = $this->getDoctrine()->getRepository('AppBundle:Students_Attendance');
+    $students = $repository->findBy(array('track_id' => $trackId));
+    return $students;
+  }
+
+  public function postStudentsAbscencePermissions(Request $request)
+  {
+
+  }
 }
