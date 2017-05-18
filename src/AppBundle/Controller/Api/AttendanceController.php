f7d2af303107d47afa7a02dde5523e3c1ff7a970
abdelrahman <abdelrehman.salah@gmail.com>
2017-05-18 11:29:28 +0200

    qr validation

--- src/AppBundle/Controller/Api/AttendanceController.php
+++ src/AppBundle/Controller/Api/AttendanceController.php
@@ -7,57 +7,70 @@ use AppBundle\Entity\Students_Absence;
 use AppBundle\Entity\Rule;
 use AppBundle\Entity\User;
 use AppBundle\Entity\Track;
+use AppBundle\Entity\QrCode;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use \Datetime;
 Use \DateTime\createFromFormat;
+date_default_timezone_set('Africa/Cairo');
 
 class AttendanceController extends FOSRestController
 {
   /**
    * @param Request $request
+   * @param string $code
    * @return array
    */
-  public function postStudentAttendanceAction(Request $request)
+  public function postStudentAttendanceAction(Request $request,$code)
   {
-      $user = $this->get('security.token_storage')->getToken()->getUser();
-      $user_id = $user->getId();
-      $trackId = $user->getTrackId();
-      $repository = $this->getDoctrine()->getRepository('AppBundle:Students_Attendance');
-      $student = $repository->findOneBy(array('userId' =>$user_id, 'trackId' => $trackId));
-      if(!$student)
+      $repository = $this->getDoctrine()->getRepository('AppBundle:QrCode');
+      $code = $repository->findOneBy(['code'=>$code]);
+      $currentDate = new DateTime();
+      $codestartdate = new DateTime($code->getStartDate()->format('Y-m-d H:i:s'));
+      $codeDuration =  $code->getDuration();
+      $codeExpirydate = $code->getStartDate()->modify('+'.$codeDuration.' seconds');
+      if ($currentDate >= $codestartdate && $currentDate <= $codeExpirydate)
       {
-        $studentAttendance = new Students_Attendance();
-        $Latedate = new DateTime();
-        $Latedate->setTime(9,15);
-        $Absencedate = new DateTime();
-        $Absencedate->setTime(10,30);
-        $currentDate = new DateTime();
-        /*dump( $Latedate);
-        dump( $currentDate);
-        dump($currentDate <= $Latedate);
-        dump($currentDate >= $Latedate && $currentDate <= $Absencedate);
-        die();*/
-        if ($currentDate <= $Latedate)
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        $user_id = $user->getId();
+        $trackId = $user->getTrackId();
+        $repository = $this->getDoctrine()->getRepository('AppBundle:Students_Attendance');
+        $student = $repository->findOneBy(array('userId' =>$user_id, 'trackId' => $trackId));
+        if(!$student)
         {
-           $studentAttendance->setStatus(1);
-        }elseif ($currentDate >= $Latedate && $currentDate <= $Absencedate)
-        {
-          $studentAttendance->setStatus(0);
+          $studentAttendance = new Students_Attendance();
+          $Latedate = new DateTime();
+          $Latedate->setTime(9,15);
+          $Absencedate = new DateTime();
+          $Absencedate->setTime(10,30);
+          /*dump( $Latedate);
+          dump( $currentDate);
+          dump($currentDate <= $Latedate);
+          dump($currentDate >= $Latedate && $currentDate <= $Absencedate);
+          die();*/
+          if ($currentDate <= $Latedate)
+          {
+             $studentAttendance->setStatus(1);
+          }elseif ($currentDate >= $Latedate && $currentDate <= $Absencedate)
+          {
+            $studentAttendance->setStatus(0);
+          }
+          $studentAttendance->setArrivalTime(new \DateTime());
+          $repository = $this->getDoctrine()->getRepository('AppBundle:Track');
+          $track = $repository->findOneById($trackId);
+          $studentAttendance->setTrack($track);
+          $studentAttendance->setUser($user);
+          $em = $this->getDoctrine()->getManager();
+          $em->persist($studentAttendance);
+          $em->flush();
+          return $this->view(['message' => 'you have signed your Attendance Successfully', 'Success' => true], Response::HTTP_CREATED);
         }
-        $studentAttendance->setArrivalTime(new \DateTime());
-        $repository = $this->getDoctrine()->getRepository('AppBundle:Track');
-        $track = $repository->findOneById($trackId);
-        $studentAttendance->setTrack($track);
-        $studentAttendance->setUser($user);
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($studentAttendance);
-        $em->flush();
-        return $this->view(['Message' => 'you have signed your Attendance Successfully', 'Success' => true], Response::HTTP_CREATED);
-      }
-      else {
-        return $this->view(['Message' => 'you have already signed your Attendance before', 'Success' => false], Response::HTTP_NOT_ACCEPTABLE);
+        else {
+          return $this->view(['message' => 'you have already signed your Attendance before', 'Success' => false], Response::HTTP_NOT_ACCEPTABLE);
+        }
+      }else {
+        return $this->view(['message' => 'Invalid QR Code','Success' => false], Response::HTTP_NOT_ACCEPTABLE);
       }
   }
 
@@ -92,7 +105,7 @@ class AttendanceController extends FOSRestController
           $repository = $this->getDoctrine()->getRepository('AppBundle:Rule');
           $rule = $repository->findOneBy(array('absenceStatus' =>'Leave With Permission'));
         }else {
-            return $this->view(['Message' => 'you can not have a leave permission','Success' => false], Response::HTTP_NOT_ACCEPTABLE);
+            return $this->view(['message' => 'you can not have a leave permission','Success' => false], Response::HTTP_NOT_ACCEPTABLE);
         }
       }
       $studentAbsence->setRule($rule);
@@ -100,9 +113,9 @@ class AttendanceController extends FOSRestController
       $em = $this->getDoctrine()->getManager();
       $em->persist($studentAbsence);
       $em->flush();
-      return $this->view(['Message' => 'your permission Submitted Successfully', 'Success' => true], Response::HTTP_CREATED);
+      return $this->view(['message' => 'your permission Submitted Successfully', 'Success' => true], Response::HTTP_CREATED);
     }else {
-      return $this->view(['Message' => 'you can not sign two permissions for the same day','Success' => false], Response::HTTP_NOT_ACCEPTABLE);
+      return $this->view(['message' => 'you can not sign two permissions for the same day','Success' => false], Response::HTTP_NOT_ACCEPTABLE);
     }
   }
 
